8247c641d71ba0c193a6c74870d5ba6f6fd92efb
kakynchik <ukakyn@gmail.com>
2025-04-05 14:31:46 +0300

    Implement City operations and update Main method

--- 1012/Program.cs
+++ 1012/Program.cs
@@ -1,9 +1,21 @@
 ﻿namespace _1012;
+using Entitie;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        City city1 = new City { CountryCount = 10 };
+        City city2 = new City { CountryCount = 20 };
+
+        City city3 = city1 + city2;
+        City city4 = city1 - city2;
+        City city5 = city1 == city2;
+        City city6 = city1 != city2;
+
+        Console.WriteLine(city3.CountryCount);
+        Console.WriteLine(city4.CountryCount);
+        Console.WriteLine(city5.CountryCount);
+        Console.WriteLine(city6.CountryCount);
     }
 }
